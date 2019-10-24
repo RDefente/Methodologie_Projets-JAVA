237e91bb2f77253d4442d166f673553add81c6a2
romain <rdefente@gmail.com>
2019-10-24 17:11:32 +0200

    added documentation and cleaned up History class

--- Todo/src/history/History.java
+++ Todo/src/history/History.java
@@ -3,22 +3,32 @@ package history;
 import java.util.ArrayList;
 
 public class History {
+    /**
+     * Attributes
+     * - linesToPrint : maximum lines to print on the history, unused
+     * - entries : representation for the changes tracked in the history
+     */
     private int linesToPrint = 20;
     private ArrayList<String> entries;
 
+    /**
+     * Constructor
+     */
     public History(){
         this.entries = new ArrayList<>();
     }
 
-    public History(int maxLines){
-        this.linesToPrint = maxLines;
-        this.entries = new ArrayList<>();
-    }
-
+    /**
+     * used for adding entries to the history
+     * @param entry : the line to add
+     */
     public void newEntry(String entry){
         this.entries.add(entry);
     }
 
+    /**
+     * used for displaying the history in a convenient way
+     */
     public void display(){
         System.out.println("History :\n");
 
